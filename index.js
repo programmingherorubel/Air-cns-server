3d441eac6b63b91f4ecf08a677029b91cac50ff2
programmingherorubel <programmingherorubel@gmail.com>
2023-06-07 02:20:27 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,89 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require('dotenv').config()
+const port = process.env.PORT || 5000
+
+// middleware
+const corsOptions = {
+  origin: '*',
+  credentials: true,
+  optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
+app.use(express.json())
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tdolxqi.mongodb.net/?retryWrites=true&w=majority`
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+})
+
+async function run() {
+  try {
+    const usersCollection = client.db('aircncDb').collection('users')
+    const roomsCollection = client.db('aircncDb').collection('rooms')
+    const bookingsCollection = client.db('aircncDb').collection('bookings')
+
+
+    // users 
+    app.put('/users/:email',async(req,res)=>{
+        const email = req.params.email 
+        const user = req.body 
+        const query = {email:email}
+        const options = {upsert:true}
+        const updateDoc = {
+            $set:user
+        }
+        const result = await usersCollection.updateOne(query,updateDoc,options)
+        console.log(result)
+        res.send(result)
+    })
+
+    // save a  room in database 
+    app.post('/rooms',async(req,res)=>{
+        const room = req.body 
+        const result = await roomsCollection.insertOne(room)
+        res.send(result)
+    }) 
+
+    app.get('/rooms',async(req,res)=>{
+        const result = await roomsCollection.find({}).toArray()
+        res.send(result)
+    })
+
+    app.get('/rooms/:id',async(req,res)=>{
+        const id = req.params.id 
+        const query = {_id:new ObjectId(id)}
+        const result = await roomsCollection.findOne(query)
+        res.send(result)
+    })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db('admin').command({ ping: 1 })
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    )
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+  res.send('AirCNC Server is running..')
+})
+
+app.listen(port, () => {
+  console.log(`AirCNC is running on port ${port}`)
+})
+
+// DB_USER=booking
+// DB_PASS=RWFBczmCtwgxx3Lz
